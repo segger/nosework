6f631526bda7ffe17be8d8712c3528ed50d027c9
segger <johanna.lynn.lahti@gmail.com>
2020-06-06 12:03:05 +0200

    Start add protocol form

--- lib/pages/protocol_page.dart
+++ lib/pages/protocol_page.dart
@@ -11,6 +11,31 @@ class ProtocolPage extends StatefulWidget {
 }
 
 class _ProtocolPageState extends State<ProtocolPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  List<DropdownMenuItem<int>> _participants = [];
+  int _selectedParticipant;
+  bool _sseValue = false;
+
+  int _totPoints = 25;
+  int _totErrors = 0;
+  int _totTime = 37000;
+
+  @override
+  void initState() {  
+    _participants.add(DropdownMenuItem(child: Text('Jojo & Kira'), value: 1,));
+    _participants.add(DropdownMenuItem(child: Text('Jojo & Niko'), value: 2,));
+
+    super.initState();
+  }
+
+  void _save() {
+    print('save protocol');
+    _formKey.currentState.save();
+    // DBRepository().saveProtocol();
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     Moment moment = ModalRoute.of(context).settings.arguments;
@@ -21,11 +46,95 @@ class _ProtocolPageState extends State<ProtocolPage> {
         actions: <Widget>[
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: IconButton(icon: Icon(Icons.check), onPressed: () { print('saving...'); },),
+            child: IconButton(
+              icon: Icon(Icons.check),
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  _save();
+                }
+              },
+            ),
           ),
         ],
       ),
-      body: Center(child: Text('hallå'),)
+      body: Form(
+        key: _formKey,
+        child: Container(
+          margin: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              _participant(),
+              _sse(),
+              _hides(),
+              _result(),
+              _comment(),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  Widget _participant() {
+    return DropdownButtonFormField<int>(
+          items: _participants,
+          value: _selectedParticipant,
+          onChanged: (value) {
+            setState(() {
+              _selectedParticipant = value;
+            });
+          },
+          decoration: InputDecoration(
+            labelText: 'Deltagare',
+          ),
+        );
+  }
+
+  Widget _sse() {
+    return CheckboxListTile(
+      title: Text('SSE'),
+      value: _sseValue,
+      onChanged: (value) {
+        setState(() {
+          _sseValue = value;
+        });
+      },
+    );
+  }
+
+  Widget _hides() {
+    return Center(child: Text('Sökområden'),);
+  }
+
+  Widget _result() {
+    return Card(
+      elevation: 4.0,
+      child: Row(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Text("Poäng"),
+              Text("$_totPoints"),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Text("Fel"),
+              Text("$_totErrors"),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Text("Total tid"),
+              Text("$_totTime"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _comment() {
+    return Center(child: Text('Kommentar'),);
   }
 }
